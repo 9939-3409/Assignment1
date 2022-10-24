25ee395b0be4a105d11d3aafaf22d10b22260e76
Sujay Gope <115613352+9939-3409@users.noreply.github.com>
2022-10-24 09:42:59 +0530

    Add files via upload

---
+++ repository/Assignment1/src/Solution2.java
@@ -0,0 +1,34 @@
+
+public class Solution2 {
+
+	public static void main(String[] args) {
+		int n=4;
+		for (int i=0; i<n; i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if (i==0)
+				System.out.print("1");
+			}
+			for(int j=0;j<n;j++)
+			{
+				if (i==1)
+				System.out.print("2");
+			}
+			for(int j=0;j<n;j++)
+			{
+				if (i==2)
+				System.out.print("3");
+			}
+			for(int j=0;j<n;j++)
+			{
+				if (i==3)
+				System.out.print("4");
+			}
+			System.out.println();
+		}
+		
+        
+	}
+
+}
